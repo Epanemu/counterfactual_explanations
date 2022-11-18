c2667847ec87fa5728781ce33914b5fd7c6aa9cb
Jiri Nemecek <contact@nemecekjiri.cz>
2022-11-18 18:24:03 +0100

    Initial variant of the entropy and WD objectives
    possibly still bugged, under development

---
+++ example_objective.py
@@ -0,0 +1,108 @@
+from nn_model import NNModel
+from counterfactual_generator import CounterfactualGenerator
+
+from data import MixedEncoder
+from objectives import compute_entropy, compute_wd
+
+import numpy as np
+import pandas as pd
+
+
+frame = pd.read_csv('adult_frame.csv')
+model_path = "model.pt"
+
+# create target binary i.e. {0,1} variable to predict
+target = np.asarray(frame['income'] == '>=50k')
+
+# extract input variables used to make prediction
+input_data = frame[frame.columns[0:8]]
+encoder = MixedEncoder(input_data)
+encoded = encoder.get_encoded_data()
+
+# train the NN
+model = NNModel(encoded.shape[1], hidden_sizes=[15, 10], output_size=1)
+model.load(model_path)
+
+# Create the explanation object and initialise it
+cf_generator = CounterfactualGenerator(model, encoder)
+
+data_size = input_data.shape[0]
+# data to compute the objective on
+
+classes = [True, False]
+class_name = {
+    True: ">=50k",
+    False: "<50k"
+}
+
+scales = np.array([c.scale for c in encoder.context])
+discrete_mask = scales == 0
+scales[discrete_mask] = 1
+
+data = []
+cf_size_data = []
+epsilons = [0.02, 0.05, 0.1, 0.2, 0.5, 1, 2]
+for sample_size in [10]:#, 50, 100, 500, 1000, 5000]:
+    for epsilon in epsilons:
+        # sample_size = int(data_size*obj_proportion)
+        # print(f"PROPORTION:{obj_proportion} ({sample_size})")
+        # print(f"EPSILON:{epsilon}")
+        # obj_proportion = 0.001
+
+        rng = np.random.default_rng(42)
+        sample_idx = rng.integers(0, data_size, sample_size)
+        input_set = input_data.iloc[sample_idx].values
+        predictions = model.predict(encoded[sample_idx]).numpy().flatten() >= 0
+
+        cf_set = cf_generator.explain_set(input_set, epsilon=epsilon, n_counterfactuals=100)
+        cf_sizes = np.array([len(x) for x in cf_set])
+        cf_size_data.append(cf_sizes)
+        n_full = (cf_sizes >= 100).sum()
+        print("Proportion of full sets:", n_full / sample_size)
+
+        input_set = input_set / scales
+        cf_diffs = []
+        for fact, cfs in zip(input_set, cf_set):
+            diffs = fact - cfs
+            diffs[:, discrete_mask] = diffs[:, discrete_mask] != 0 # one for every changed feature.
+            cf_diffs.append(diffs)
+        cf_set = np.array(cf_set, dtype=object) # they are different length, must be treated as objects
+        cf_diffs = np.array(cf_diffs, dtype=object) # they are different length, must be treated as objects
+
+        for i, context in enumerate(encoder.context):
+            # print(f"{context.name}: ")
+            bin_edges = None
+            for output_class in classes:
+                # print(f"For class {class_name[output_class]}")
+                mask = predictions == output_class
+
+                data_entropy, bin_edges = compute_entropy(input_set[mask, i], is_categorical=(context.scale==0), bin_edges=bin_edges)
+                # other
+                cf_entropy, _ = compute_entropy(np.concatenate(cf_set[~mask])[:, i], is_categorical=(context.scale==0), bin_edges=bin_edges)
+                # print(f"Difference of entropy of original and counterfactuals: {data_entropy - cf_entropy:.2f}")
+
+                cf_diff_entropy, _ = compute_entropy(np.concatenate(cf_diffs[mask])[:, i], is_categorical=(context.scale==0))
+
+                # wasserstein distance of distribution of countefactualy generated points to the distribution of the original points
+                wasserstein = compute_wd(np.concatenate(cf_set[~mask])[:, i], input_set[mask, i], is_categorical=(context.scale==0), bin_edges=None)
+                data.append({
+                    "sample_size": sample_size,
+                    "epsilon": epsilon,
+                    "class": class_name[output_class],
+                    "feature": context.name,
+                    "diff_of_entropies": data_entropy - cf_entropy,
+                    "entropy_of_changes": cf_diff_entropy,
+                    "wasserstein_d": wasserstein,
+                    "n_maxed_cfs": n_full
+                })
+                # print(f"Entropy of difference to factual: {cf_diff_entropy:.2f}")
+                # print()
+        #     print()
+        # print()
+    size_result = pd.DataFrame(cf_size_data)
+    size_result.insert(0, "epsilons", epsilons)
+    size_result.to_csv(f"export/cf_sizes_{sample_size}.csv", index=None)
+    cf_size_data = []
+
+result = pd.DataFrame(data)
+result.to_csv("export/results_all.csv", index=None)
---
+++ objectives.py
@@ -0,0 +1,112 @@
+"""
+Utility function based on wasserstein distance between current and goal distributions.
+
+Example use:
+
+Have some data and classifier
+classify the data
+for each estimated class, compute distribution over the features of the input
+
+compute set of closest counterfactuals for all the classified points
+for the set of counterfactuals and for each class, compute distribution over their features
+
+compute optimal transport for each feature distribution between classes
+
+"""
+import ot
+
+def compute_wd(from_data, to_data, is_categorical, bin_edges=None):
+    if is_categorical:
+        from_unique, from_u_counts = np.unique(from_data, return_counts=True)
+        to_unique, to_u_counts = np.unique(to_data, return_counts=True)
+
+        # unify the distributions
+        from_counts = []
+        to_counts = []
+        tot_unique = np.unique(np.concatenate([from_unique, to_unique]))
+        # print("UNIQUES")
+        # print(tot_unique)
+        # print(from_unique)
+        # print(to_unique)
+        for u_value in tot_unique:
+            from_mask = from_unique == u_value
+            if np.any(from_mask):
+                from_counts.append(from_u_counts[from_mask][0])
+            else:
+                from_counts.append(0)
+            to_mask = to_unique == u_value
+            if np.any(to_mask):
+                to_counts.append(to_u_counts[to_mask][0])
+            else:
+                to_counts.append(0)
+
+
+        from_counts = np.array(from_counts, dtype=np.float64)
+        to_counts = np.array(to_counts, dtype=np.float64)
+
+        n = tot_unique.shape[0]
+        M = np.ones((n,n)) # all distances are equal
+    else:
+        if bin_edges is None:
+            from_counts, bin_edges = np.histogram(from_data, bins="fd") # compute the histogram using Freedman Diaconis Estimator for bin width
+        else:
+            from_counts, bin_edges = np.histogram(from_data, bins=bin_edges)
+        to_counts, _ = np.histogram(to_data, bins=bin_edges)
+        # print("DATA")
+        # print(bin_edges)
+        # print(from_data)
+        # print(to_data)
+
+        bin_centers = bin_edges[:-1] + (bin_edges[1:] - bin_edges[:-1]) / 2
+        M = ot.dist(bin_centers.reshape((-1, 1)), bin_centers.reshape((-1, 1)))
+        if M.max() != 0:
+            M /= M.max()
+    # print(M)
+    # print(from_counts, to_counts)
+    # print(from_counts / from_counts.sum(), to_counts / to_counts.sum())
+    return ot.emd2(from_counts / from_counts.sum(), to_counts / to_counts.sum(), M) # emd2 returns the Earth Mover Distance loss
+
+
+"""
+Utility function based on difference of entropies between current and goal distributions.
+
+Example use:
+
+Have some data and classifier
+classify the data
+for each estimated class, compute distribution over the features of the input
+
+compute set of closest counterfactuals for all the classified points
+for the set of counterfactuals and for each class, compute distribution over their features
+
+compute entropy of all distributions
+compute the difference between counterfactual distribution and classified distribution for each class
+
+(or compute the entropy of a distribution on some difference of (average or median or sth) datapoints.)
+(or compute entropy of distance of counterfactuals to the original datapoint (weighted by the distance to optimum?), some kind of robustness measure?)
+
+"""
+
+from scipy.stats import entropy
+import numpy as np
+
+def compute_entropy(data, is_categorical=False, bin_edges=None):
+    """
+    Takes one dimensional numpy array of size N
+        and bool is_categorical that is True if the data is categorical
+    If parameter bin_edges is set, this will be the the bins used, otherwise a new estimate will be made.
+
+    returns the entropy of the corresponding (histogram estimated) distribution and the bin edges used (None if unused)
+    """
+    if is_categorical:
+        _, counts = np.unique(data, return_counts=True)
+    else:
+        if bin_edges is None:
+            counts, bin_edges = np.histogram(data, bins="fd") # compute the histogram using Freedman Diaconis Estimator for bin width
+        else:
+            counts, bin_edges = np.histogram(data, bins=bin_edges)
+
+
+    return entropy(counts), bin_edges # entropy function normalizes the data to sum to 1
+
+
